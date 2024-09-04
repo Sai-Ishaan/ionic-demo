36d12dc5450b9a9145d3d0fad8e91fb3762815d9
DocWrath07 <saiishaanbala04@gmail.com>
2024-09-04 19:51:29 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic_Demo',
+  webDir: 'dist'
+};
+
+export default config;
