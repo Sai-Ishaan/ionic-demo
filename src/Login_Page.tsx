619abffe5c248b9439cd1c5dfd55c291384c4de1
DocWrath07 <saiishaanbala04@gmail.com>
2024-09-07 00:01:52 +0530

    modified:   src/Login.css 	modified:   src/Login_Page.tsx

--- src/Login_Page.tsx
+++ src/Login_Page.tsx
@@ -1,65 +1,90 @@
-import React,{useState} from "react";
-import{
-    IonContent,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonButton,
-    IonIcon,
-    IonItem,
-    IonLabel,
-    IonToast,
-    IonInput,
-    IonPage
+import React, {useState} from 'react';
+
+import {
+  IonPage,
+  IonContent,
+  IonInput,
+  IonButton,
+  IonItem,
+  IonLabel,
+  IonCheckbox,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonIcon
 } from '@ionic/react';
+import { logoGoogle, logoFacebook} from 'ionicons/icons';
 import './Login.css';
-const Login:React.FC=()=>{
-    const [username, setUsername] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+
+const Login: React.FC = ()=> {
+    const [email, setEmail] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
     const [showToast, setShowToast] = useState(false);
-    
-    const handleLogin = ()=>{
-        if (username==="admin" && password==='password'){
-            setShowToast(true);
-        }
-        else{
-            alert('Invalid Credentials! Try again :)');
+    const [rememberMe, setRememberMe] = useState<boolean>(false);
+   const handleLogin = () =>{
+       if(username==="admin" && password==="password"){
+           setShowToast(true);
+   }  
+      else{
+       alert('Invalid Credentials! Try again');
+   } 
+ };
 
-        }
-    };
+return(
+   <IonPage>
+       <IonContent className="login-content">
+          <div className="login-box">
 
-    return(
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                      <IonTitle>Login</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-          <IonContent className="paddin">
-                 <IonItem>
-                    <IonLabel position="stacked">Username</IonLabel>
-                    <IonInput type="text" value={username} onIonChange={e => setUsername(e.detail.value!)}
-                    clearInput
-                    />             
-                    </IonItem>
-                  <IonItem>
-                    <IonLabel position="stacked">Password</IonLabel>
-                        <IonInput
-                           type="password"
-                           value={password}
-                           placeholder="Enter your password"
-                           onIonChange={e => setPassword(e.detail.value!)}
-                           clearInput
-                        />   </IonItem>   
-                        <IonButton expand="full" onClick={handleLogin} className="margin-top">Login</IonButton>
-                        <IonToast
-                        isOpen={showToast}
-                        onDidDismiss={() => setShowToast(false)}
-                        message="Login Successful"
-                        duration={2000}/>
-                    </IonContent>           
-        </IonPage>
-    );
+            <div className="logo">
+               <IonIcon icon={logoGoogle}/>
+            </div> 
+               <IonText className="login-header">Welcome, User!</IonText>
+              <IonGrid>
+                  <IonRow className="ion-justify-content-center">
+                  <IonCol size="auto">
+                     <IonButton fill="outline" className="socials">
+                     <IonIcon slot="start" icon={logoGoogle}/>
+                       Google
+                     </IonButton>
+                   </IonCol>
+                   <IonCol size="auto">
+                     <IonButton fill="outline" className="socials">
+                     <IonIcon slot="start" icon={logoFacebook}/>
+                       Facebook
+                     </IonButton>
+                   </IonCol>
+                  </IonRow>    
+              </IonGrid>
+
+         <IonText className="or-text">or</IonText>
+            <IonItem>
+             <center>  <IonLabel position="floating">Email address or Username</IonLabel></center> 
+             <center>  <IonInput type="text" value={username} onIonChange={e => setUsername(e.detail.value!)}/></center><br/>
+             <center>   <IonLabel position="floating">Password</IonLabel></center>
+             <center>  <IonInput type="password" value={password} onIonChange={(e)=>setPassword(e.detail.value!)}/></center>
+            </IonItem>
+          <div className="options">
+          <IonCheckbox
+              checked={rememberMe}
+              className='remember-me'
+              onIonChange={(e) => setRememberMe(e.detail.checked!)}
+            >Remember Me</IonCheckbox>
+           <IonText className="forgot-password">Forgot Password</IonText>
+          </div>
+             <IonButton expand="block" className="login-btn" onClick={handleLogin}>
+             Sign In
+             </IonButton>
+             <br/>
+             <br/>
+             <IonText className="create-account">
+                  Not registered yet? Try Signing Up!<a href="/signup" >Create an Account</a>
+             </IonText>
+          </div>
+       </IonContent>
+   </IonPage>
+ );
 };
 
 export default Login;
